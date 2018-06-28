99b230fbf7134b7186ae78334127ffcc668cef3c
vic1507 <vicy94@hotmail.it>
2018-06-28 20:46:02 +0200

    Add files via upload
    Added a lot of lambda for a better code, eliminated useless structures like Map and other that was not necessary

--- itwiki.spark/src/main/java/itwiki/spark/Sparkalo.java
+++ itwiki.spark/src/main/java/itwiki/spark/Sparkalo.java
@@ -8,7 +8,6 @@ import java.util.Map;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.PairFunction;
 import org.apache.spark.sql.SparkSession;
 
 import scala.Tuple2;
@@ -18,66 +17,33 @@ public class Sparkalo {
 	public static void main (String [] args)
 	{
 		
-		SparkSession spark = SparkSession.builder().appName("Simple Application").getOrCreate();
+		SparkSession sparkSession = SparkSession.builder().appName("Simple Application").getOrCreate();
 		
-		JavaSparkContext ctx = new JavaSparkContext(spark.sparkContext());
+		JavaSparkContext context = new JavaSparkContext(sparkSession.sparkContext());
 
-		JavaRDD<String> lines = ctx.textFile("author", 1);
-		
-		JavaPairRDD<String, String> pairs = lines.mapToPair((final String s) -> {
-		    String[] tokens = s.split("\t"); 
-		    return new Tuple2<String, String>(tokens[4], tokens[2]);
-		});
+		JavaRDD<String> inputFile = context.textFile("author", 1);
+		JavaPairRDD<String, String> pairs = inputFile.mapToPair(row -> new Tuple2<String, String>(row.split("\t")[4], row.split("\t")[2]));
 		
 		Map<String, Iterable<String>> map = pairs.groupByKey().collectAsMap();
-		Map<String, Map<String,Integer>> results = new HashMap<>();
+		Map<String, JavaPairRDD<Integer, String> > results = new HashMap<>();
 		
-		for (String s : map.keySet())
+		for (String author : map.keySet())
 		{
-			List<Tuple2<String, Integer>> tuples = new ArrayList<Tuple2<String,Integer>>();
-			for (String s2 : map.get(s))
-			{
-				tuples.add(new Tuple2<String, Integer>(s2, 1));
-			}
+			List<Tuple2<String, Integer>> tuplesToCount = new ArrayList<Tuple2<String,Integer>>();
+			for (String category : map.get(author))
+				tuplesToCount.add(new Tuple2<String, Integer>(category, 1));
 			
-			JavaPairRDD<String,Integer> pairRDD = ctx.parallelizePairs(tuples);
-			JavaPairRDD<Integer, String> swappedPair = pairRDD.mapToPair(new PairFunction<Tuple2<String, Integer>, Integer, String>() {
-		           @Override
-		           public Tuple2<Integer, String> call(Tuple2<String, Integer> item) throws Exception {
-		               return item.swap();
-		           }
-
-		        });
-			
-			JavaPairRDD<Integer, String> sortByKey = ctx.parallelizePairs(swappedPair.sortByKey(false).take(5));
-			JavaPairRDD<String,Integer> ordered = sortByKey.mapToPair(new PairFunction<Tuple2<Integer, String>, String, Integer>() {
-		           @Override
-		           public Tuple2<String,Integer> call(Tuple2<Integer,String> item) throws Exception {
-		               return item.swap();
-		           }
-
-		        });
-			
-			Map<String, Integer> reduceByKey = ordered.reduceByKey((i1,i2)-> i1+i2).collectAsMap();
-			results.put(s, reduceByKey);
-			
+			JavaPairRDD<String,Integer> tuplesToCountPair = context.parallelizePairs(tuplesToCount);
+			JavaPairRDD<Integer, String> swappedPairSortByKey =context.parallelizePairs(tuplesToCountPair.reduceByKey((i1,i2)-> i1+i2).mapToPair(item -> item.swap()).sortByKey(false).take(5));
+			results.put(author, swappedPairSortByKey);
 		}
 		
-		for (String s : results.keySet())
+		for (String author : results.keySet())
 		{
-			int i = 0;
-			for (String s2 :results.get(s).keySet())
-			{
-				if (i>4)
-					break;
-				System.out.println(s + " mod " + s2 + " : " + results.get(s).get(s2).toString() + " times");
-				i++;
-			}
+			results.get(author).foreach(line -> System.out.println("Author : " + author + "Category : " + line._2 + " Value :" + line._1));
 		}
 		
-//		ops.collect().forEach(t -> System.out.println(t._1 + "         " + t._2));
-			
-		ctx.close();
-		spark.stop();
+		context.close();
+		sparkSession.stop();
 	}
 }
