e5a1443e8c4873ed929349ff38f69e99f8b3fb29
vic1507 <vicy94@hotmail.it>
2018-06-29 10:53:53 +0200

    Added print on file
    Added some lambda for improving the qualitiy of the code

--- itwiki.spark/src/main/java/itwiki/spark/Sparkalo.java
+++ itwiki.spark/src/main/java/itwiki/spark/Sparkalo.java
@@ -1,49 +1,57 @@
 package itwiki.spark;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
 import org.apache.spark.api.java.JavaPairRDD;
-import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.SparkSession;
-
 import scala.Tuple2;
 
 public class Sparkalo {
-	
-	public static void main (String [] args)
-	{
+
+	public static void main(String[] args) {
+
+		File out = new File("RISULTATISPARK");
+		writeOnFile("Autore" +"\t" + "Categoria"+"\t"+"Num modifiche", out);
 		
 		SparkSession sparkSession = SparkSession.builder().appName("Simple Application").getOrCreate();
-		
 		JavaSparkContext context = new JavaSparkContext(sparkSession.sparkContext());
 
-		JavaRDD<String> inputFile = context.textFile("author", 1);
-		JavaPairRDD<String, String> pairs = inputFile.mapToPair(row -> new Tuple2<String, String>(row.split("\t")[4], row.split("\t")[2]));
-		
-		Map<String, Iterable<String>> map = pairs.groupByKey().collectAsMap();
-		Map<String, JavaPairRDD<Integer, String> > results = new HashMap<>();
-		
-		for (String author : map.keySet())
-		{
-			List<Tuple2<String, Integer>> tuplesToCount = new ArrayList<Tuple2<String,Integer>>();
-			for (String category : map.get(author))
+		JavaPairRDD<String, String> pairs = context.textFile("author", 1)
+				.mapToPair(row -> new Tuple2<String, String>(row.split("\t")[4], row.split("\t")[2]));
+
+		for (Tuple2<String, Iterable<String>> author : pairs.groupByKey().collect()) {
+			List<Tuple2<String, Integer>> tuplesToCount = new ArrayList<Tuple2<String, Integer>>();
+			for (String category : author._2)
 				tuplesToCount.add(new Tuple2<String, Integer>(category, 1));
-			
-			JavaPairRDD<String,Integer> tuplesToCountPair = context.parallelizePairs(tuplesToCount);
-			JavaPairRDD<Integer, String> swappedPairSortByKey =context.parallelizePairs(tuplesToCountPair.reduceByKey((i1,i2)-> i1+i2).mapToPair(item -> item.swap()).sortByKey(false).take(5));
-			results.put(author, swappedPairSortByKey);
+
+			context.parallelizePairs(context.parallelizePairs(tuplesToCount).reduceByKey((i1, i2) -> i1 + i2)
+					.mapToPair(item -> item.swap()).sortByKey(false).take(5))
+					.foreach(line -> writeOnFile(author._1 + " \t" + line._2 + "\t" + line._1, out));
 		}
-		
-		for (String author : results.keySet())
-		{
-			results.get(author).foreach(line -> System.out.println("Author : " + author + "Category : " + line._2 + " Value :" + line._1));
-		}
-		
+
 		context.close();
 		sparkSession.stop();
 	}
+	
+	static void writeOnFile(String resultsSingleRow, File out) {
+
+		FileWriter fw;
+
+		try {
+			fw = new FileWriter(out, true);
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write(resultsSingleRow + "\n");
+			bw.close();
+			fw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
 }
